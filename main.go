64b83bc48f945e1eb6f1e747da1b45eb2d0b99af
o-sokol-o <sokol.pad@gmail.com>
2022-08-15 13:28:30 +0300

    add config

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 	"weatherstack/weatherstack"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -14,8 +16,19 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Инициализация читалки конфига
+	viper.SetConfigName("config")   // name of config file (without extension)
+	viper.SetConfigType("yaml")     // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("configs/") // path to look for the config file in
+	err = viper.ReadInConfig()      // Find and read the config file
+	if err != nil {                 // Handle errors reading the config file
+		log.Fatalf("fatal error config file: %+v", err)
+	}
+
 	// Получаем JWT токен (если регистрация валидна)
-	err = weatherClient.Login("toxin-air", "rOTlZqgDseCk")
+	l := viper.GetString("weather.login")
+	p := viper.GetString("weather.password")
+	err = weatherClient.Login(l, p)
 	if err != nil {
 		log.Fatalf("Authorization error")
 	}
